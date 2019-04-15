bc975f7e292b10296ca590a936f4e601943be4e6
manjinder1998 <47332868+manjinder1998@users.noreply.github.com>
2019-04-15 17:07:02 -0400

    Add files via upload

---
+++ mapscript1.js
@@ -0,0 +1,7 @@
+var map;
+function initialize(){
+ var mapOptions = {
+     zoom:12,
+     center: new google.maps.Map(document.getElemenyById('map_canvas'),mapOptions);
+}
+google.maps.event.addDomListener(window,'load',initialize);
